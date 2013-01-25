b2f4000d6b5373d1a3cd663768b190cb610d45cf
ichbinder <ichbinder@ubuntu>
2013-01-25 11:14:38 -0800

    first

---
+++ src/scene.h
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//	scene.h
+//	Draw scene for shadow mapping
+//	Downloaded from: www.paulsprojects.net
+//	Created:	16th September 2003
+//
+//	Copyright (c) 2006, Paul Baker
+//	Distributed under the New BSD Licence. (See accompanying file License.txt or copy at
+//	http://www.paulsprojects.net/NewBSDLicense.txt)
+//////////////////////////////////////////////////////////////////////////////////////////	
+
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <stdio.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include "Maths/Maths.h"
+
+void DrawScene(float angle);
+
+#endif	//SCENE_H
